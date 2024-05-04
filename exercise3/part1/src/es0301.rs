c559026f58d84243439f60d18d65c1e2f84a3661
Gabriele Iannace <gabriele.iannace@gmail.com>
2024-05-04 18:34:30 +0200

    [ex3][part1]: subsequence4

--- exercise3/part1/src/es0301.rs
+++ exercise3/part1/src/es0301.rs
@@ -118,7 +118,7 @@ pub fn demo2() {
 // 4. Spoiler: basically it's not possible to return more then one mutable reference to the same data
 // 5. Try this workaround: return a vector of indexes (first solution) and let the caller extract the mutable references
 // 7. (later in the course you will learn about smart pointers, which can be used to solve this kind of problems in a more elegant way)
-fn subsequences3<'a, 'b>(s: &'a mut str, seq: &'b str) -> Vec<(usize, usize)> {
+fn subsequences3(s: &mut str, seq: &str) -> Vec<(usize, usize)> {
     let mut v = Vec::new();
     let mut start_index = 0;
     loop {
@@ -147,23 +147,34 @@ pub fn demo3() {
     }
 }
 
-// // DNA strings may be very long and we can get a lot of matches.
-// // Therefore we want to process a subsequence as soon as we find it, without storing it in a vector
-// // A solution is to pass a closure to the function, which will be called for each match
-// // do you need to put lifetime annotations in the closure? why?
-// fn subsequence4(s: &str, seq: &str /* add your closure here */) {
-//     unimplemented!()
-// }
-//
-// pub fn demo4() {
-//     let a = "AACGGTAACC".to_string();
-//     let seq = "A1-1,C2-4";
-//
-//     subsequence4(&a, seq, |pos, sub| {
-//         println!("Found subsequence at position {}: {}", pos, sub);
-//     });
-// }
-//
+// DNA strings may be very long and we can get a lot of matches.
+// Therefore we want to process a subsequence as soon as we find it, without storing it in a vector
+// A solution is to pass a closure to the function, which will be called for each match
+// do you need to put lifetime annotations in the closure? why?
+fn subsequence4<F: Fn(usize, &str)>(s: &str, seq: &str, closure: F){
+    let mut indice = 0;
+    loop{
+        match  find_sub(&s[indice..], seq){
+            None => {break;}
+            Some(result) => {
+                indice = result.0+result.1.len();
+                closure(result.0, result.1);
+            }
+        }
+    }
+
+}
+
+#[test]
+pub fn demo4() {
+    let a = "AACGGTAACC".to_string();
+    let seq = "A1-2,C2-4";
+
+    subsequence4(&a, seq, |pos, sub| {
+        println!("Found subsequence at position {}: {}", pos, sub);
+    });
+}
+
 // // Now let's define a struct SimpleDNAIter (add the required lifetimes), memorizing a DNA sequence and the subsequence to search
 // // Then we add a next() method to the struct, which will return the next subsequence found in the DNA sequence after each call
 // // The result of next() is a tuple, but it's wrapped in an Option, because a call to next() may find no more subsequences in the DNA sequence
